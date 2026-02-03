9f4be25feb48f16336eba61e8365ec3e3986c835
grmc1999 <grmc1999@gmail.com>
2026-02-03 18:29:13 -0300

    readme

--- src/learners.py
+++ src/learners.py
@@ -161,12 +161,12 @@ class ModelOCIFAR10Dynamics(DynamicSampler):
             self.y_train.append(y.to(self.device))
 
 
-class ModelOCIFAR10Dynamics(DynamicSampler):
+class ModelOMNISTDynamics(DynamicSampler):
     def __init__(self, model_instance, device='cpu', num_train_samples=1000):
         super().__init__(model_instance, device, num_train_samples)
 
     #def _load_cifar10(self, num_samples):
-    def _load_mnist(self, num_samples: int) -> None:
+    def _load_dataset(self, num_samples: int) -> None:# implemented with cifar10
         transform = transforms.Compose(
             [
                 transforms.ToTensor(),
