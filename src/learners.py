9c3e8e0527c7982598320e7e81c8fd749f4ad00d
grmc1999 <grmc1999@gmail.com>
2026-02-04 12:54:26 -0300

    add

--- src/learners.py
+++ src/learners.py
@@ -209,7 +209,7 @@ class ModelOMNISTDynamics(DynamicSampler):
             [
                 transforms.ToTensor(),
                 transforms.Grayscale(num_output_channels=3),
-                transforms.Normalize((0.1307,0.1307,0.1307,), (0.3081,0.3081,0.3081,)),  # standard MNIST mean/std
+                #transforms.Normalize((0.1307,0.1307,0.1307,), (0.3081,0.3081,0.3081,)),  # standard MNIST mean/std
             ]
         )
 
