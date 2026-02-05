c3d54336565d6cd59863d8e0d391d7fc13c82b54
guillermo <gmorenoc@uni.pe>
2026-02-05 20:26:50 -0300

    lenet

--- src/dynamic_models.py
+++ src/dynamic_models.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 from  torchvision.models import alexnet
 from  torchvision import models
+import numpy as np
 
 class Residualblock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
@@ -96,3 +97,38 @@ efficientnet_b0_10 = lambda: models.efficientnet_b0(num_classes = 10)
 efficientnet_b3_10 = lambda: models.efficientnet_b3(num_classes = 10)
 efficientnet_b5_10 = lambda: models.efficientnet_b5(num_classes = 10)
 efficientnet_b7_10 = lambda: models.efficientnet_b7(num_classes = 10)
+
+
+
+class LeNet(nn.Module):
+
+    # network structure
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5, padding=2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1   = nn.Linear(16*5*5, 120)
+        self.fc2   = nn.Linear(120, 84)
+        self.fc3   = nn.Linear(84, 10)
+
+    def forward(self, x):
+        '''
+        One forward pass through the network.
+        
+        Args:
+            x: input
+        '''
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        '''
+        Get the number of features in a batch of tensors `x`.
+        '''
+        size = x.size()[1:]
+        return np.prod(size)
